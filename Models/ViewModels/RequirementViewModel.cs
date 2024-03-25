5171a206349cbb6630c4db4337c311637babf0d9
croftn <nyatcroft@gmail.com>
2024-03-25 16:51:06 -0600

    joseph is the best person I've ever met

---
+++ Models/ViewModels/RequirementViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using ToaPro.Models;
+
+namespace ToaPro.Models.ViewModels
+{
+    public class RequirementViewModel
+    {
+        public List<Requirement> Class1Requirements { get; set; } = new List<Requirement>();
+        public List<Requirement> Class2Requirements { get; set; } = new List<Requirement>();
+        public List<Requirement> Class3Requirements { get; set; } = new List<Requirement>();
+        public List<Requirement> Class4Requirements { get; set; } = new List<Requirement>();
+    }
+}
