ebb7fad6c92b9bb646f58dea3beb30ef7b7afba4
ethannance <ethannance523@gmail.com>
2024-03-25 16:01:20 -0600

    Attempted to greate the post method but didn't finish

--- Controllers/StudentSubmissionsController.cs
+++ Controllers/StudentSubmissionsController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ToaPro.Models;
 
 namespace ToaPro.Controllers
 {
     public class StudentSubmissionsController : Controller
     {
+
+        private IStudentsRepository _repo;
+
+        public StudentSubmissionsController(IStudentsRepository temp) //Constructor
+        {
+            _repo = temp;
+        }
         public IActionResult Index()
         {
             return View();
@@ -19,9 +27,27 @@ namespace ToaPro.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult StudentSubmitFiles()
         {
+
             return View("StudentSubmitFiles");
+        }
+
+        [HttpPost]
+        public IActionResult StudentSubmitFiles(Submission response)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.StudentSubmitFiles(response);
+
+                return View("StudentSubmitFilesConfirmation", response);
+            }
+            else
+            {
+                ViewBag.Categories = _repo.Categories.OrderBy(x => x.CategoryName).ToList();
+                return View(response);
+            }
         }
 
     }
