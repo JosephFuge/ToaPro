6e051142dbfd4ec838dc8cda53a7775e89b182e6
Bookworm2035 <126933809+Bookworm2035@users.noreply.github.com>
2024-03-25 14:48:36 -0600

    Add psudo code for PresntationSchedlue action

--- Controllers/PresentationsController.cs
+++ Controllers/PresentationsController.cs
@@ -11,6 +11,13 @@ namespace ToaPro.Controllers
 
         public IActionResult PresentationSchedule()
         {
+            //Croordinatior view shows Judges, Prof, Group, and rooms, times. Can edit table! 
+
+            //Judge View shows the room number time and judge name, along with the group number. Can request new time
+
+            //Prof view shows the room number, time, and prof name, along with the group number
+
+
             //Add functionality to load the presentation page based on user type (Coord, Judge, Prof)
             //Coordinator can edit presentation schedule and judges can request new timeslots
             return View();
