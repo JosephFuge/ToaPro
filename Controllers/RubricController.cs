38ddb9edd4a883b040181c7a45e3f858dbe50f48
jd1010756 <106291794+j1010756@users.noreply.github.com>
2024-03-27 13:47:30 -0600

    new table

--- Controllers/RubricController.cs
+++ Controllers/RubricController.cs
@@ -27,7 +27,7 @@ namespace ToaPro.Controllers
                 Class3Requirements = requirements.Where(r => r.ClassId == 3).ToList(),
                 Class4Requirements = requirements.Where(r => r.ClassId == 4).ToList()
             };
-
+            
             return View(viewModel);
         }
 
