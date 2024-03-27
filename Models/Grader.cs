7b609870a6b0dc5c038649ebf0bec1547c6d298c
jherm <jherm@JENSENSLAPTOP>
2024-03-27 14:32:35 -0600

    I edited the Grader Table.

--- Models/Grader.cs
+++ Models/Grader.cs
@@ -1,23 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ToaPro.Models;
 
 public partial class Grader
 {
     public int Id { get; set; }
+    [ForeignKey("Name")]
+    public int NameId { get; set; }
 
-    public int ClassId { get; set; }
-
-    public string FName { get; set; } = null!;
-
-    public string LName { get; set; } = null!;
-
-    public string NetId { get; set; } = null!;
-
-    public bool IsProfessor { get; set; }
-
+    [ForeignKey("Requirement")]
+    public int Requirement_ID { get; set; }
+    [ForeignKey("Group")]
+    public int Group_ID { get; set; }
+    public virtual Requirement Requirement { get; set; }
+    public virtual Group Group { get; set; }
     public virtual Class Class { get; set; } = null!;
+    public virtual Name Name { get; set; }
 
     public virtual ICollection<Grade> Grades { get; set; } = new List<Grade>();
 
