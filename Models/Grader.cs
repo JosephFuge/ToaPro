f3fc85f7a097a548b2ac88a425c11e4a3318e339
maurineiko <nohaskett@gmail.com>
2024-03-28 10:21:54 -0600

    commenting out the name table for a second in the grader model

--- Models/Grader.cs
+++ Models/Grader.cs
@@ -14,10 +14,10 @@ public partial class Grader
     public int Requirement_ID { get; set; }
     [ForeignKey("Group")]
     public int Group_ID { get; set; }
-    public virtual Requirement Requirement { get; set; }
-    public virtual Group Group { get; set; }
+    public virtual Requirement? Requirement { get; set; }
+    public virtual Group? Group { get; set; }
     public virtual Class Class { get; set; } = null!;
-    public virtual Name Name { get; set; }
+    // public virtual Name Name { get; set; }
 
     public virtual ICollection<Grade> Grades { get; set; } = new List<Grade>();
 
