6e89dd287da6fc7ab3d6886dcab16c4952a6d1b5
btoone <braden@toonetown.com>
2024-05-14 18:58:47 -0600

    Renamed UserRolesViewModel.cs to UsersViewModel.cs; Simplified storage of map between roles and users (potentially space inefficient for users with multiple roles?); Removed unnecessary attributes due to JQ usage in view;

---
+++ Models/ViewModels/UsersViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToaPro.Models.ViewModels
+{
+    public class UsersViewModel
+    {
+        [Required]
+        public IDictionary<string, IList<ToaProUser>> RoleUsers { get; set; } = new Dictionary<string, IList<ToaProUser>>();
+
+        [Required]
+        public IList<Student> Students { get; set; } = new List<Student>();
+    }
+}
